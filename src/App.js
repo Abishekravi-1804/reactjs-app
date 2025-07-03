09d165274b4c13903ffbf3daed54f415c8450ad3
Abishek Ravi <abishekaswin1804@gmail.com>
2025-07-03 12:53:45 +0530

    Initial commit: React app for Docker + Railway

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>🚀 Hello from React on Fly.io!</h1>
-      <p>This is a simple app deployed using Docker.</p>
+      <h1> Hello from React & Railway</h1>
+      <p>This is a simple app deployed using Docker image </p>
     </div>
   );
 }
