af274b1fdbae47ab2fe34e2b0bd6073ff1f87785
Abishek Ravi <abishekaswin1804@gmail.com>
2025-07-03 14:05:30 +0530

    Initial commit: Calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Calculator from './Calculator';
+
 
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1> Hello from React & Railway</h1>
-      <p>This is a simple app deployed using Docker image </p>
+    <div className="App">
+      <h1>Calculator</h1>
+      <Calculator />
     </div>
   );
 }
